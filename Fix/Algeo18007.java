4ab3f9a5bc662f10a003505a452d5ed874e5250b
Ikhsan010 <54568146+Ikhsan010@users.noreply.github.com>
2019-09-27 09:10:31 +0700

    Create Algeo18007.java

---
+++ Fix/Algeo18007.java
@@ -0,0 +1,18 @@
+//package src;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Algeo18007 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        MATRIKS matriks = new MATRIKS();
+        Eksternal ex = new Eksternal();
+        Menu menu = new Menu();
+        //matriks = ex.bacaFileMatriks("../src/coba.txt");
+        //menu.Menu();
+        menu.bacaMenu();
+    }
+}
+        
